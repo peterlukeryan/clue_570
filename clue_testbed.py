7bd4943e0a74c766a41306e7ce9e68f6585fb7a4
Peter Ryan <147430628+peterryan12@users.noreply.github.com>
2025-02-03 15:20:53 -0600

    Fix duplicated bug

--- clue_testbed.py
+++ clue_testbed.py
@@ -3,7 +3,7 @@ import random
 # Initialize characters, weapons, rooms
 characters = ["Dr. Orchid", "Mr. Green", "Col. Mustard", "Ms. Peacock", "Prof. Plum","Ms. Scarlett"]
 weapons = ["Wrench", "Rope", "Steel Bar", "Knife", "Shovel", "Razor"]
-rooms = ["Hall", "Piano Room", "Greenhouse", "Study", "Billiard Room", "Bedroom", "Dining Room", "Dining Room", "Library", "Kitchen"]
+rooms = ["Hall", "Piano Room", "Greenhouse", "Study", "Billiard Room", "Bedroom","Dining Room", "Library", "Kitchen"]
 
 # Randomly select three cards, from each type-- as 'murder cards'
 murder_cards = []
@@ -20,5 +20,14 @@ rooms.remove(murder_room)
 pooled_cards = characters + weapons + rooms
 random.shuffle(pooled_cards)
 
+#Initialize 'player hands'
+print(len(pooled_cards))
+player_one_hand = pooled_cards[0:6]
+player_two_hand = pooled_cards[6:12]
+player_three_hand = pooled_cards[12:18]
+
+print(player_one_hand)
+print(player_two_hand)
+print(player_three_hand)
 
 
