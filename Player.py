e803a3c2c6331d34ac908359d4b26f80977a82cc
Peter Ryan <147430628+peterryan12@users.noreply.github.com>
2025-02-04 18:41:28 -0600

    Basic implementation of Player abstract class

---
+++ Player.py
@@ -0,0 +1,20 @@
+class Player:
+    def __init__(self, player_hand=None, suspect_list=None):
+        self.player_hand = player_hand if player_hand is not None else []
+        self.suspect_list = suspect_list if suspect_list is not None else []
+
+    def show_hand(self):
+        return f"Player's hand: {self.player_hand}"
+
+    def show_suspect_list(self):
+        return f"Suspect list: {self.suspect_list}"
+
+    def response(self, accused_cards):
+        matching_cards = list(set(self.player_hand) & set(accused_cards))
+        if not matching_cards:
+            return ""
+        else:
+            return matching_cards[0]
+
+    def accuse(self, player, cards):
+        return player.response(cards)
