d42c2366dadc46a423a415897b5909ffb0b3ed3e
Peter Ryan <147430628+peterryan12@users.noreply.github.com>
2025-02-10 13:38:19 -0600

    Delete comments

--- clue_testbed.py
+++ clue_testbed.py
@@ -75,37 +75,3 @@ while not game_over:
         game_over = True
 
 
-
-# player_one_hand = pooled_cards[0:6]
-#
-# # Create distinct arrays for each card type
-# player_one_characters = list(set(player_one_hand) & set(characters))
-# player_one_weapons = list(set(player_one_hand) & set(weapons))
-# player_one_rooms = list(set(player_one_hand) & set(rooms))
-#
-#
-# #create distinct arrays for each suspect type
-# p1_character_suspects = [character for character in characters if character not in player_one_characters]
-# p1_weapon_suspects = [weapon for weapon in weapons if weapon not in player_one_weapons]
-# p1_room_suspects = [room for room in rooms if room not in player_one_rooms]
-#
-#
-#
-# player_two_hand = pooled_cards[6:12]
-# player_three_hand = pooled_cards[12:18]
-#
-#
-#
-# #Introduce 'suspect list'-- i.e all cards player-one doesn't know about
-# suspect_list = [x for x in pooled_cards if x not in player_one_hand]
-#
-#
-# player1 = Player(player_hand = [player_one_characters, player_one_weapons, player_one_rooms], suspect_list = [p1_character_suspects, p1_weapon_suspects, p1_room_suspects])
-# print(player1.show_hand())
-# print(player1.show_suspect_list())
-#
-# player2 = Player(player_hand = player_two_hand, suspect_list = [p1_character_suspects, p1_weapon_suspects, p1_room_suspects])
-#
-#
-# print(player1.accuse(player2))
-
